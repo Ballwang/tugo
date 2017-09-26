a415e453f49995cf3e075764a5383a10bec064f0
jiangshu <wangyinneng@jsnews.net.cn>
2017-09-26 15:03:37 +0800

    D6

--- D6-Category.go
+++ D6-Category.go
@@ -14,11 +14,11 @@ import (
 
 var completeCat chan int = make(chan int)
 var textLen = 24 //8个汉字
-var serverIDCategory="D6-Catgory"
-var serverCategoryPort=8085
+var serverIDCategory = "D6-Category"
+var serverCategoryPort = 8085
 
 //过滤列表页链接
-func FilterCategoryList(w http.ResponseWriter, req *http.Request)  {
+func FilterCategoryList(w http.ResponseWriter, req *http.Request) {
 
 	for {
 		c, _ := tool.NewRedisCluster()
@@ -26,6 +26,7 @@ func FilterCategoryList(w http.ResponseWriter, req *http.Request)  {
 		for {
 			SiteList := []string{}
 			for i := 0; i < config.MaxProcess; i++ {
+				//for i := 0; i < 1; i++ {
 				reply, _ := c.Do("LPOP", config.UpdateList)
 				if reply != nil {
 					SiteList = append(SiteList, string(reply.([]byte)))
@@ -35,11 +36,12 @@ func FilterCategoryList(w http.ResponseWriter, req *http.Request)  {
 					break
 				}
 			}
+
 			process := len(SiteList)
 			if process > 0 {
 				for _, v := range SiteList {
 					//进入并发程序
-					go GetUrlFromCategory(c,v)
+					go GetUrlFromCategory(c, v)
 				}
 				for i := 0; i < process; i++ {
 					<-completeCat
@@ -49,21 +51,20 @@ func FilterCategoryList(w http.ResponseWriter, req *http.Request)  {
 			if IsEnd == 1 {
 				break
 			}
-			tool.SetServerState(serverIDCategory,"5")
-
+			tool.SetServerState(serverIDCategory, "5")
+			//break
 		}
-
-		tool.SetServerState(serverIDCategory,"5")
+		//break
+		tool.SetServerState(serverIDCategory, "5")
 
 		c.Close()
 		time.Sleep(1 * time.Second)
 	}
-	
 }
 
 //返回服务状态
-func ServerCategoryState(w http.ResponseWriter, req *http.Request)  {
-	fmt.Fprint(w,tool.GetServerState(serverIDCategory))
+func ServerCategoryState(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, tool.GetServerState(serverIDCategory))
 }
 
 //列表页详细链接提取
@@ -72,20 +73,22 @@ func main() {
 	ip := tool.GetIP()
 	http.HandleFunc("/FilterCategoryList", FilterCategoryList)
 	http.HandleFunc("/State", ServerCategoryState)
-	register:=&tool.ConsulRegister{Id:serverIDCategory,Name:"D6-列表页链接提取服务！",Port:serverCategoryPort,Tags:[]string{"列表页链接提取服务！自动识别提取列表页链接！"}}
+	register := &tool.ConsulRegister{Id: serverIDCategory, Name: "D6-列表页链接提取服务！", Port: serverCategoryPort, Tags: []string{"列表页链接提取服务！自动识别提取列表页链接！"}}
 	register.RegisterConsulService()
 	err := http.ListenAndServe(ip+":"+strconv.Itoa(serverCategoryPort), nil)
 
 	if err != nil {
 		fmt.Println("Listen And Serve error: ", err.Error())
 	}
-
 }
 
+
 //从栏目页提取链接
-func GetUrlFromCategory(c *redis.Cluster,url string) {
+func GetUrlFromCategory(c *redis.Cluster, url string) {
 
 	content, _ := c.Do("GET", config.PrefixCategory+url)
+
+	//fmt.Println(config.PrefixCategory+url)
 	if content != nil {
 		//获取链接提取规则
 		urlString, _ := c.Do("HGET", config.MonitorShowHash, url)
@@ -134,6 +137,8 @@ func GetUrlFromCategory(c *redis.Cluster,url string) {
 				}
 			}
 		}
+
+		//os.Exit(-1)
 
 		//分析链接特征
 		count := map[int]int{}
@@ -191,11 +196,11 @@ func GetUrlFromCategory(c *redis.Cluster,url string) {
 				}
 			}
 
-
 			result := []string{}
 			if len(countMap) > 0 && maxLen >= config.MinNumOfUrl {
 				for k, v := range countMap {
 					//兼容网页侧边栏相似链接现象
+
 					isDo := false
 					if (maxLen - secondLen) <= config.MaxMinusValue {
 						if v == maxKey || v == secondKey {
@@ -209,8 +214,9 @@ func GetUrlFromCategory(c *redis.Cluster,url string) {
 					if isDo {
 						if maxkeySubfix != "" {
 							if strings.Contains(k, maxkeySubfix) {
-								result = append(result, k)
+								//result = append(result, k)
 								AbsoluteUrl := tool.GetAbsoluteUrl(url, k)
+								result = append(result, AbsoluteUrl)
 								historyKey := tool.GetHostUri(url)
 								md5String := tool.Md5String(AbsoluteUrl)
 								key := config.HistoryPrefix + historyKey
@@ -220,6 +226,7 @@ func GetUrlFromCategory(c *redis.Cluster,url string) {
 									c.Do("SADD", key, md5String)
 									c.Do("HSET", config.ContentParentHash, AbsoluteUrl, urlParent)
 									c.Do("SADD", config.ContentUrlSet, AbsoluteUrl)
+									SetUpdateTime(c, urlParent)
 								}
 								if historyKey == "" {
 									c.Do("SADD", config.NullKey, url)
@@ -229,10 +236,23 @@ func GetUrlFromCategory(c *redis.Cluster,url string) {
 							c.Do("SADD", config.NullUrlInCategory, urlParent)
 						}
 					}
+
 				}
 			}
+			//fmt.Println(result)
 
 		}
 	}
 	completeCat <- 1
+}
+
+//记录采集条数历史
+func SetUpdateTime(c *redis.Cluster, url string) {
+	t := time.Now()
+	y := t.Year()
+	m := t.Month().String()
+	d := t.Day()
+	h := t.Hour()
+	s := "count:" + strconv.Itoa(y) + "-" + m + "-" + strconv.Itoa(d) + "-" + strconv.Itoa(h)
+	c.Do("HINCRBY", s, url, 1)
 }
