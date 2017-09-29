026b3eaf91e827c872e497d54a8f3359c1661636
jiangshu <wangyinneng@jsnews.net.cn>
2017-09-29 18:05:12 +0800

    更新自动注册

--- config/config.go
+++ config/config.go
@@ -1,6 +1,5 @@
 package config
 
-
 import (
 	"bufio"
 	"io"
@@ -11,16 +10,16 @@ import (
 const middle = "-"
 
 type Config struct {
-	ConfigMap  map[string]string
-	strcet string
+	ConfigMap map[string]string
+	strcet    string
 }
 
 //初始化Config，配置文件地址是写程序的相对地址
 func NewConfig() *Config {
 	//分配内存
-	c:=new(Config)
+	c := new(Config)
 	c.ConfigMap = make(map[string]string)
-    path:="./config/config.ini"
+	path := "./config/config.ini"
 	f, err := os.Open(path)
 	if err != nil {
 		panic(err)
@@ -46,7 +45,7 @@ func NewConfig() *Config {
 		n1 := strings.Index(s, "[")
 		n2 := strings.LastIndex(s, "]")
 		if n1 > -1 && n2 > -1 && n2 > n1+1 {
-			c.strcet = strings.TrimSpace(s[n1+1 : n2])
+			c.strcet = strings.TrimSpace(s[n1+1: n2])
 			continue
 		}
 
@@ -87,16 +86,15 @@ func NewConfig() *Config {
 		if len(second) == 0 {
 			continue
 		}
-		key:=c.strcet+middle+frist
-		c.ConfigMap[key]=strings.TrimSpace(second)
+		key := c.strcet + middle + frist
+		c.ConfigMap[key] = strings.TrimSpace(second)
 	}
 	return c
 }
 
-
 //获取配置文件详细信息
 func (c *Config) GetConfig(section string, mapKey string) string {
-	key:=section+middle+mapKey
+	key := section + middle + mapKey
 	v, found := c.ConfigMap[key]
 	if !found {
 		return ""
